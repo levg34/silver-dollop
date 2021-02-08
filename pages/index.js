3ffe66e601096f08c89222b0b39263f44f56cf34
levg34 <levg34@users.noreply.github.com>
2021-02-08 21:44:34 +0100

    setup nextjs

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+function HomePage() {
+    return <>
+        <Head>
+            <title>Silver dollop</title>
+        </Head>
+        <h1>Silver dollop</h1>
+    </>
+}
+
+export default HomePage
