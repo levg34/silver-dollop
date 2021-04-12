79914e96c8ca11630990ac8d74ee771f095ec0e0
levg34 <levg34@users.noreply.github.com>
2021-04-12 23:40:38 +0200

    add several users for test

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Container from 'react-bootstrap/Container'
+import { getUserList } from '../data/utils'
 
 function HomePage() {
+    const users = getUserList()
+
     return <>
         <Head>
             <title>Silver dollop</title>
         </Head>
         <Container>
             <h1>Silver dollop</h1>
-            <Link href="/users/luc">Luc</Link>
+            {users && users.map(user => <><Link href={'/users/'+user}>{`View ${user}'s data`}</Link><br/></>)}
         </Container>
     </>
 }
